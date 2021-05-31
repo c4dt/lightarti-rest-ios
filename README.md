632498e2d4260bf2037c6b170baa14bc35432294
Linus Gasser <linus.gasser@epfl.ch>
2021-05-31 21:58:53 +0200

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# arti-ios
+
+A description of this package.
---
+++ Tests/arti-iosTests/arti_iosTests.swift
@@ -0,0 +1,11 @@
+    import XCTest
+    @testable import arti_ios
+
+    final class arti_iosTests: XCTestCase {
+        func testExample() {
+            // This is an example of a functional test case.
+            // Use XCTAssert and related functions to verify your tests produce the correct
+            // results.
+            XCTAssertEqual(arti_ios().text, "Hello, World!")
+        }
+    }
