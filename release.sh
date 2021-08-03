232201d13a4e33a57d22ff7a66463afa84c993e4
Linus Gasser <linus.gasser@epfl.ch>
2021-08-03 07:57:48 +0200

    Checking git-state and committing changes to Swift.package

--- release.sh
+++ release.sh
@@ -26,16 +26,27 @@ get_next_tag(){
   get_next "$( echo "$RELEASE" | jq -r '.tag_name')" "$( git tag | tail -n 1)"
 }
 
+# Check if the repo is committed
+if [ -n "$( git status --porcelain )" ]; then
+	echo "Please commit changes first"
+	exit 1
+fi
+
 # Get latest release of arti rest and update the Package.swift with the
 # hash and the url
 RELEASE=$( curl -sL https://api.github.com/repos/c4dt/arti-rest/releases/latest )
 XCFRAMEWORK_URL=$( echo "$RELEASE" | jq -r '.assets[].browser_download_url')
-CHK=$(curl -s "$XCFRAMEWORK_URL" | sha256sum | cut -d' ' -f1)
+echo URL is: $XCFRAMEWORK_URL
+CHK=$(curl -s -L "$XCFRAMEWORK_URL" | sha256sum | cut -d' ' -f1)
+echo Checksum is: $CHK
 sed -i '' -e "s=\(\\s*url: \).* \(// XCFramework URL.*\)=\1\"$XCFRAMEWORK_URL\", \2=;
        s=\(\\s*checksum: \).* \(// XCFramework checksum.*\)=\1\"$CHK\" \2=" \
        Package.swift
 
 # Get the next tag
 NEWTAG=$( get_next_tag "$RELEASE" )
-echo "Next tag will be: $NEWTAG"
+git commit -qam "Tagging $NEWTAG"
+echo "Tagging release with: $NEWTAG"
 git tag "$NEWTAG"
+echo "Please check everything is OK and then use"
+echo "git push --tags"
